8eec4a07809fb03c412c4b831c406043ece59a3c
ヨシダタイキ <abc521fjr7@gmail.com>
2021-04-13 20:41:39 +0900

    [修正]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a1964ba07f8289b9189a0ef0b5777e952f3d2d3abfa90537a5e3f19ca32c52d5bdaa54a7e6641f42c91d08a58bfb221854f1d96e65957d6135fea54d3adcfcd1'
