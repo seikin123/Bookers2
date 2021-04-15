d9d8652bcb3890bd1797b0403e6f0be505f85a2d
ヨシダタイキ <abc521fjr7@gmail.com>
2021-04-15 14:33:53 +0900

    [add]edit current_userを指定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @user = current_user
@@ -9,13 +10,18 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @books = @user.books
     @booknew = Book.new
-    
   end
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      render "edit"
+  else
+    redirect_to user_path(current_user)
   end
+end
 
   def update
     @user = User.find(params[:id])
